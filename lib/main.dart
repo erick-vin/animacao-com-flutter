2a66b23a3c33cc4001791569c41cf822bccc03fe
Erick Vinicius <ericktjdev@gmail.com>
2021-12-03 11:19:48 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:animacoes/AnimacaoImplicita.dart';
+import 'package:animacoes/AnimacaoTween.dart';
+import 'package:animacoes/AnimacoesExplicitasConstruidas.dart';
+import 'package:animacoes/CriandoAnimacoesBasicas.dart';
+import 'package:animacoes/MaisSobreAnimacoes.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: MaisSobreAnimacoes(),
+    debugShowCheckedModeBanner: false,
+  )
+);
